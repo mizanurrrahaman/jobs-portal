ddbb1d9bc8757bf676b7f5a9e06b9fe482ccd7a9
km-saifullah <kmsaifullah16@gmail.com>
2024-01-01 11:50:29 +0600

    header section done

---
+++ src/components/commonBtn/CommonBtn.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./commonBtn.scss";
+
+const CommonBtn = ({ title }) => {
+  return <button className="common_btn">{title}</button>;
+};
+
+export default CommonBtn;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Navbar from "./navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "./footer/Footer";
+import Header from "./header/Header";
 
 const MainLayout = () => {
   return (
     <>
       <Navbar />
+      <Header />
       <Outlet />
       <Footer />
     </>
