cf089b910d9cd5f95d78f28930aa926abc5c4bf9
km-saifullah <kmsaifullah16@gmail.com>
2024-01-06 10:58:25 +0600

    popular vacancies section done

--- src/sections/popularVacancies/Vacancies.jsx
+++ src/sections/popularVacancies/Vacancies.jsx
@@ -3,8 +3,21 @@ import "./vacancies.scss";
 
 const Vacancies = ({ vacancyData }) => {
   console.log(vacancyData);
-  const { title, Anesthesiologists, Surgeons, Obstetricians_Gynecologists } =
-    vacancyData;
+  const {
+    title,
+    Anesthesiologists,
+    Surgeons,
+    Obstetricians_Gynecologists,
+    Orthodontists,
+    MaxillofacialSurgeons,
+    SoftwareDeveloper,
+    Psychiatrists,
+    DataScientist,
+    FinancialManager,
+    ManagementAnalysis,
+    ITManager,
+    OperationsResearchAnalysis,
+  } = vacancyData;
   return (
     <section className="vacancies">
       <div className="container">
@@ -19,12 +32,44 @@ const Vacancies = ({ vacancyData }) => {
             <p>{Surgeons.openPositions} Open Piositions</p>
           </div>
           <div className="vacancy_item">
-            <h4>{Anesthesiologists.title}</h4>
-            <p>{Anesthesiologists.openPositions} Open Piositions</p>
+            <h4>{Obstetricians_Gynecologists.title}</h4>
+            <p>{Obstetricians_Gynecologists.openPositions} Open Piositions</p>
           </div>
           <div className="vacancy_item">
-            <h4>{Anesthesiologists.title}</h4>
-            <p>{Anesthesiologists.openPositions} Open Piositions</p>
+            <h4>{Orthodontists.title}</h4>
+            <p>{Orthodontists.openPositions} Open Piositions</p>
+          </div>
+          <div className="vacancy_item">
+            <h4>{MaxillofacialSurgeons.title}</h4>
+            <p>{MaxillofacialSurgeons.openPositions} Open Piositions</p>
+          </div>
+          <div className="vacancy_item">
+            <h4>{SoftwareDeveloper.title}</h4>
+            <p>{SoftwareDeveloper.openPositions} Open Piositions</p>
+          </div>
+          <div className="vacancy_item">
+            <h4>{Psychiatrists.title}</h4>
+            <p>{Psychiatrists.openPositions} Open Piositions</p>
+          </div>
+          <div className="vacancy_item">
+            <h4>{DataScientist.title}</h4>
+            <p>{DataScientist.openPositions} Open Piositions</p>
+          </div>
+          <div className="vacancy_item">
+            <h4>{FinancialManager.title}</h4>
+            <p>{FinancialManager.openPositions} Open Piositions</p>
+          </div>
+          <div className="vacancy_item">
+            <h4>{ManagementAnalysis.title}</h4>
+            <p>{ManagementAnalysis.openPositions} Open Piositions</p>
+          </div>
+          <div className="vacancy_item">
+            <h4>{ITManager.title}</h4>
+            <p>{ITManager.openPositions} Open Piositions</p>
+          </div>
+          <div className="vacancy_item">
+            <h4>{OperationsResearchAnalysis.title}</h4>
+            <p>{OperationsResearchAnalysis.openPositions} Open Piositions</p>
           </div>
         </div>
       </div>
