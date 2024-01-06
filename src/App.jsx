74f4dd95cf6297d6cfb28920bea6e8b40acec7a8
mizanurrrahaman <rodking1414@gmail.com>
2024-01-05 21:07:29 -0800

    About add

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,14 @@ import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/home/Home";
 import Jobs from "./pages/jobs/Jobs";
 import Dashbord from "./pages/dashbord/Dashbord";
+import About from "./pages/about/About";
 
 // All Routes
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<MainLayout />}>
       <Route path="/" element={<Home />} />
+       <Route path="/about" element={<About/>}/>
       <Route path="/jobs" element={<Jobs />} />
       <Route path="/dashbord" element= {<Dashbord/>}/>
     </Route>
---
+++ src/pages/about/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./about.scss"
+
+
+const About = () => {
+  return (
+    <section className="about">
+      <div className="container">
+         <div className="about-heading">
+            <h5>About us</h5>
+            <h5>Home/About us</h5>
+         </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
